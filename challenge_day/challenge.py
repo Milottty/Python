59bb7becab083a56e174034489bb96f325de92fc
Milottty <milotymeri1@gmail.com>
2026-04-03 22:57:11 +0200

    challenge

--- challenge_day/challenge.py
+++ challenge_day/challenge.py
@@ -8,20 +8,27 @@ class Person:
         def get_name(self):
             return self.name
 
-        def set_name(self, name):
-            self.name = name
+        def set_name(self, new_name):
+            self.name = new_name
 
         def get_age(self):
             return self.age
 
-        def set_age(self, age):
-            self.age = age
+        def set_age(self, new_age):
+            self.age = new_age
 
         def get_weight(self):
             return self.weight
 
-        def set_weight(self, weight):
-            self.weight = weight
+        def set_weight(self, new_weight):
+            self.weight = new_weight
+
+        def get_height(self):
+            return self.height
+
+        def set_height(self, new_height):
+            self.height = new_height
+
 
         
 
@@ -33,7 +40,7 @@ class Person:
     def get_bmi_category(self):
         pass
     def print_info(self):
-        print(f"{self.name} eshte {self.age} vjec, peshon {self.weight} dhe eshte i gjat{self.height}")
+        print(f"{self.name} eshte {self.age} vjec, peshon {self.weight} dhe eshte i gjat {self.height}")
 
 
 class Adult(Person):
@@ -68,3 +75,60 @@ class Child(Person):
             return "Over Weight"
         else:
             return "Obese"
+
+
+while True:
+    user_name= input("Enter your name: ")
+
+    if user_name.isalpha():
+        print("Valid input:", user_name)
+        break
+    else:
+        print("Try again! No numbers allowed.")
+
+while True:
+    user_age = input("Enter your age: ")
+
+    if user_age.isdigit():
+        print("Valid number:", user_age)
+        break
+    else:
+        print("Try again! Only numbers allowed.")
+
+while True:
+    user_height = input("Enter your Height (In Centimeters): ")
+
+    if user_height.isdigit():
+        print("Valid number:", user_height)
+        break
+    else:
+        print("Try again! Only numbers allowed.")
+
+while True:
+    user_weight = input("Enter your Weight (In Kilograms): ")
+
+    try:
+        user_weight = float(user_weight)
+
+        if user_weight > 0:
+            print("Valid number:", user_weight)
+            break
+        else:
+            print("Your weight needs to be positive. Try again.")
+
+    except ValueError:
+        print("Enter a number")
+
+user_age = int(user_age)
+user_height = float(user_height)
+
+if user_age < 18:
+    person = Child(user_name, user_age, user_weight, user_height)
+else:
+    person = Adult(user_name, user_age, user_weight, user_height)
+
+bmi = person.bmi_cal()
+print("BMI:", bmi)
+
+category = person.get_bmi_category()
+print("Category:", category)
